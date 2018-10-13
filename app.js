523ad0d47a91aa6a25fc174c76fe4440245951a8
Daniel Khan <daniel.khan@dynatrace.com>
2018-10-14 01:05:38 +0200

    New Deployment

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 const appzip = require('appmetrics-zipkin')({
   host: 'localhost',
   port: 9411,
-  serviceName:'voting-service-gateway',
+  serviceName:'voting-service-blue',
   sampleRate: 1.0
 });
 
