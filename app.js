4b99b2a5904accff083db359ac20f9f8bb664379
Daniel Khan <daniel.khan@dynatrace.com>
2018-10-16 11:11:14 +0200

    New Deployment

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/demo-votes');
 const indexRouter = require('./routes/index');
 
 const app = express();
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+
 router.get('/', (req, res, next) => {
-  // return res.status(500).end();
-  res.render('index', { title: 'Green' });
+
 });
 
 module.exports = router;
