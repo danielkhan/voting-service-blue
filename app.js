62c1a2a98c007e9e93f4c6cea6f8fa4c6b073819
Daniel Khan <daniel@khan.io>
2021-02-17 00:45:56 +0100

    jaeger setup

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-const zipkin = require('./agent/zipkin')('service-blue');
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/votings');
-const indexRouter = require('./routes/index');
+const tracing = require("./agent/tracing");
+tracing.init("service-blue");
+
+const tracer = tracing.getTracer("service-blue");
+tracing.enableConsoleExporter();
+tracing.enableJaegerExporter();
+
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/votings");
+const indexRouter = require("./routes/index");
 
 const app = express();
-app.use(zipkin.middleware());
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -31,11 +36,11 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
