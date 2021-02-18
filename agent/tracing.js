5a6a6815b4a0d651d8ede742c292c8a2288a5fce
Daniel Khan <daniel@khan.io>
2021-02-18 01:42:29 +0100

    remove init

--- agent/tracing.js
+++ agent/tracing.js
@@ -13,18 +13,16 @@ const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
 let _serviceName = "";
 
 const provider = new NodeTracerProvider();
-provider.register();
-module.exports.init = (serviceName) => {
-  _serviceName = serviceName;
-  provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+_serviceName = "service-green";
+provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
 
-  // Enable jaeger exporter
-  const options = {
-    serviceName: _serviceName,
-  };
-  const jaegerExporter = new JaegerExporter(options);
-  provider.addSpanProcessor(new SimpleSpanProcessor(jaegerExporter));
+// Enable jaeger exporter
+const options = {
+  serviceName: _serviceName,
 };
+const jaegerExporter = new JaegerExporter(options);
+provider.addSpanProcessor(new SimpleSpanProcessor(jaegerExporter));
+provider.register();
 
 module.exports.getTracer = (name) => {
   return opentelemetry.trace.getTracer(name);
